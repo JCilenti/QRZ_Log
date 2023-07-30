147dc132a2ca11a9472463e6d574f4a956e8c899
Joseph Cilenti <jecilenti@gmail.com>
2023-07-29 21:33:33 -0400

    Login complete

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome()
-driver.get('https://www.qrz.com/db/')
-id_box = driver.find_element('xpath', '//*[@id="cs"]')
-#id_box = driver.find_element("name", "query")
-id_box.send_keys('KO4KRC')
-time.sleep(5)
-search = driver.find_element('xpath', '//*[@id="qform"]/table/tbody/tr[1]/td[2]/input')
-search.click()
+driver.get('https://www.qrz.com/login')
+user_id_box = driver.find_element('xpath', '//*[@id="username"]')
+user_id_box.send_keys('jecilenti@gmail.com')
+next_button = driver.find_element('xpath', '//*[@id="login-form"]/div/div[2]/div[3]/div/div[2]/button')
+next_button.click()
+time.sleep(8)
+pswd_box = driver.find_element('xpath', '//*[@id="password"]')
+pswd_box.send_keys('WestPoint1322')
+sign_in_button = driver.find_element('xpath', '//*[@id="login-form"]/div/div[2]/div[4]/div/div[2]/button')
+sign_in_button.click()
+time.sleep(10)
+
