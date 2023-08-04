d6e59ea48674c36a40a443555de6b739abf4319f
Joseph Cilenti <jecilenti@gmail.com>
2023-08-03 22:26:19 -0400

    Added some formatting

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from datetime import date
 import sys
 import csv
 
+divider = "*" * 50
+
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 
@@ -27,25 +29,33 @@ sign_in_button = driver.find_element('xpath', '//*[@id="login-form"]/div/div[2]/
 sign_in_button.click()
 time.sleep(10) # was 10
 call_sign_search = driver.find_element('xpath', '//*[@id="tquery"]')
-call_sign_search.send_keys('KO4EXI')
+call_sign_search.send_keys('KO4EXI') # this needs to be an argument passed by the user
 cs_search_button = driver.find_element('xpath', '//*[@id="tsubmit"]')
 cs_search_button.click()
 time.sleep(5)
 detail_button = driver.find_element('xpath', '//*[@id="ui-id-2"]')
 detail_button.click()
 time.sleep(5)
+
 user_data_main = driver.find_element(By.CLASS_NAME, 'm0')
 user_text = user_data_main.text
+
 user_data_class = driver.find_element(By.XPATH, '//*[@id="dt"]/tbody/tr[4]/td[2]')
 class_text = user_data_class.text
+
 user_data_grid = driver.find_element(By.XPATH, '//*[@id="dt"]/tbody/tr[7]/td[2]')
 grid_text = user_data_grid.text
+
 show_map = driver.find_element(By.XPATH, '//*[@id="dbmap"]/input')
 #new_text = user_text.strip('\n')
 new_text = user_text.splitlines()[0]
+
 print(new_text)
+print(divider)
 print(class_text)
+print(divider)
 print(grid_text)
+print(divider)
 print("Time =", current_time)
 print("Date =", d)
 show_map.click()
